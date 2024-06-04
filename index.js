7fe893a952ca32b17c273e89ef1e88cce8b78c46
mutanim24 <mutanim24@gmail.com>
2024-06-04 20:26:34 +0600

    updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 const app = express()
 const port = 3000
@@ -24,8 +24,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const allRecipes = client.db("tanim-kitchen").collection("all-recipes");
+
+    app.get('/recipes', async (req, res) => {
+      const result = await allRecipes.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/recipe/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allRecipes.findOne(query);
+      res.send(result);
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
